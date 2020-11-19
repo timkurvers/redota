3ad63a29580bebc73e78d54696a0de475786d2e3
Tim Kurvers <tim@moonsphere.net>
2020-11-19 19:12:33 +0100

    fix: expose Button component in index

--- src/ui/components/index.js
+++ src/ui/components/index.js
@@ -1,4 +1,5 @@
 export { default as Bar } from './Bar.jsx';
+export { default as Button } from './Button.jsx';
 export { default as Link } from './Link.jsx';
 export { default as PlayerColor } from './PlayerColor.jsx';
 export {
