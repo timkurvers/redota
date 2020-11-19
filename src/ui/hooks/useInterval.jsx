4d7ca05ee50f1397ddf8770d3c9dda19e116761c
Tim Kurvers <tim@moonsphere.net>
2020-11-19 19:11:05 +0100

    feat: add useInterval hook

---
+++ src/ui/hooks/useInterval.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useRef } from 'react';
+
+// See: https://overreacted.io/making-setinterval-declarative-with-react-hooks/
+const useInterval = (callback, delay) => {
+  const savedCallback = useRef();
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    const tick = () => {
+      savedCallback.current();
+    };
+    if (delay !== null) {
+      const id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+    return undefined;
+  }, [delay]);
+};
+
+export default useInterval;
