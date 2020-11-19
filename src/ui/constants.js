084324773c6718d5cb6f396b4ea05eea542e4b75
Tim Kurvers <tim@moonsphere.net>
2020-11-19 19:14:47 +0100

    feat: add temporary team and player constants

---
+++ src/ui/constants.js
@@ -0,0 +1,22 @@
+export const TEAM_RADIANT = 2;
+export const TEAM_DIRE = 3;
+export const TEAM_NEUTRAL = 4;
+export const TEAM_COLORS = {
+  [TEAM_RADIANT]: '#4D8A2F',
+  [TEAM_DIRE]: '#FF1700',
+  [TEAM_NEUTRAL]: '#000000',
+};
+
+// TODO: Needs a mapping by player ID
+export const PLAYER_COLORS = [
+  '#3375FF',
+  '#66FFBF',
+  '#BF00BF',
+  '#F3F00B',
+  '#FF6B00',
+  '#FE86C2',
+  '#A1B447',
+  '#65D9F7',
+  '#008321',
+  '#A46900',
+];
