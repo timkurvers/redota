ebcf777e8041ba81a53a48f35dfc1460785d4e3b
Tim Kurvers <tim@moonsphere.net>
2020-11-24 21:17:24 +0100

    fix: ensure portrait always reserves space to prevent HUD from jumping

--- src/ui/hud/selection/Portrait.jsx
+++ src/ui/hud/selection/Portrait.jsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import { HeroResource } from '../../components/index.js';
 
 const StyledPortrait = styled.div`
+  width: 138px;
   cursor: pointer;
 
   & img {
