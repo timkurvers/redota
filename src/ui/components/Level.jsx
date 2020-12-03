3b849850ff5b326fb5fb252c41d5e3dbaead4b3f
Tim Kurvers <tim@moonsphere.net>
2020-12-03 15:34:40 +0100

    fix: correct constants import

--- src/ui/components/Level.jsx
+++ src/ui/components/Level.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
 
-import { xpPerLevel } from '../../lib/dotaconstants.js';
+import { xpPerLevel } from '../../lib/constants.js';
 
 const StyledLevel = styled.div`
   position: relative;
