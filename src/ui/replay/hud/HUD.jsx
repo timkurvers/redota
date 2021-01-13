ce27f02d61c65ac8db2608b97328fc41929217ad
Tim Kurvers <tim@moonsphere.net>
2021-01-13 16:32:43 +0100

    docs: clarify what the HUD component is used for

--- src/ui/replay/hud/HUD.jsx
+++ src/ui/replay/hud/HUD.jsx
@@ -4,12 +4,13 @@ export { default as Selection } from './selection/Selection.jsx';
 export { default as Timeline } from './timeline/Timeline.jsx';
 export { default as Topbar } from './topbar/Topbar.jsx';
 
+// Exists solely to clearly indicate HUD components in ReplayView
 const HUD = (props) => {
   const { children } = props;
   return (
-    <div>
+    <>
       {children}
-    </div>
+    </>
   );
 };
 
