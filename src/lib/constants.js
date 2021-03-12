21f12d929293de185cddfdf96787dd07aca259f9
Tim Kurvers <tim@moonsphere.net>
2021-03-12 14:11:36 +0100

    feat: assign color to unknown players

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -6,18 +6,19 @@ export const MAP_SIZE = 32768;
 export const MAP_HALF_SIZE = MAP_SIZE / 2;
 export const CELL_SIZE = 128;
 
-export const PLAYER_COLORS = [
-  '#3375FF',
-  '#66FFBF',
-  '#BF00BF',
-  '#F3F00B',
-  '#FF6B00',
-  '#FE86C2',
-  '#A1B447',
-  '#65D9F7',
-  '#008321',
-  '#A46900',
-];
+export const PLAYER_COLORS = {
+  [-1]: '#DDDDDD',
+  0: '#3375FF',
+  1: '#66FFBF',
+  2: '#BF00BF',
+  3: '#F3F00B',
+  4: '#FF6B00',
+  5: '#FE86C2',
+  6: '#A1B447',
+  7: '#65D9F7',
+  8: '#008321',
+  9: '#A46900',
+};
 
 export const GAME_PHASE = {
   PREGAME: 4,
