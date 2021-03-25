7c9ae05ce4d1db4a0942264d3d10692af2488ebc
Tim Kurvers <tim@moonsphere.net>
2021-03-25 15:22:38 +0100

    fix: correct CDN and locally bundled resource references

--- src/lib/resources.js
+++ src/lib/resources.js
@@ -1,28 +1,29 @@
-const BUNDLED_BASE_URL = '/images';
-const CDN_BASE_URL = 'https://steamcdn-a.akamaihd.net/apps/dota2/images';
+const BUNDLED_BASE_URL = './images';
+
+const CDN_BASE_URL = 'https://cdn.cloudflare.steamstatic.com/apps/dota2/images';
 
 export const abilityResourceFor = (refname) => (
-  // Example: http://steamcdn-a.akamaihd.net/apps/dota2/images/abilities/winter_wyvern_winters_curse_md.png
-  `${CDN_BASE_URL}/abilities/${refname}_md.png`
+  // Example: https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/abilities/winter_wyvern_winters_curse.png
+  `${CDN_BASE_URL}/dota_react/abilities/${refname}.png`
 );
 
 export const heroResourceFor = (refname, variant) => {
   let suffix = 'full.png';
   switch (variant) {
     case 'portrait':
-      // Example: https://steamcdn-a.akamaihd.net/apps/dota2/images/heroes/phoenix_vert.jpg
+      // Example: https://cdn.cloudflare.steamstatic.com/apps/dota2/images/heroes/phoenix_vert.jpg
       suffix = 'vert.jpg';
       break;
     case 'landscape':
-      // Example: https://steamcdn-a.akamaihd.net/apps/dota2/images/heroes/oracle_sb.png
+      // Example: https://cdn.cloudflare.steamstatic.com/apps/dota2/images/heroes/oracle_sb.png
       suffix = 'sb.png';
       break;
     case 'icon':
-      // Example: https://steamcdn-a.akamaihd.net/apps/dota2/images/heroes/shadow_shaman_icon.png
+      // Example: https://cdn.cloudflare.steamstatic.com/apps/dota2/images/heroes/shadow_shaman_icon.png
       suffix = 'icon.png';
       break;
     default:
-      // Example: https://steamcdn-a.akamaihd.net/apps/dota2/images/heroes/jakiro_full.png
+      // Example: https://cdn.cloudflare.steamstatic.com/apps/dota2/images/heroes/jakiro_full.png
       suffix = 'full.png';
       break;
   }
@@ -31,7 +32,7 @@ export const heroResourceFor = (refname, variant) => {
 };
 
 export const itemResourceFor = (refname) => {
-  // Example: https://steamcdn-a.akamaihd.net/apps/dota2/images/items/blink_lg.png
+  // Example: https://cdn.cloudflare.steamstatic.com/apps/dota2/images/items/blink_lg.png
   let itemname = refname.replace('item_', '');
   // TODO: Overrides should preferably not be in the UI
   if (itemname.startsWith('recipe_')) {
@@ -41,17 +42,17 @@ export const itemResourceFor = (refname) => {
 };
 
 export const teamLogoResourceFor = (teamID) => (
-  // Example: https://steamcdn-a.akamaihd.net/apps/dota2/images/team_logos/36.png
+  // Example: https://cdn.cloudflare.steamstatic.com/apps/dota2/images/team_logos/36.png
   `${CDN_BASE_URL}/team_logos/${teamID}.png`
 );
 
 export const unitResourceFor = (refname, variant) => {
   switch (variant) {
     case 'portrait':
-      // Example: /images/portraits/npc_dota_roshan.jpg
+      // Example: ./images/portraits/npc_dota_roshan.jpg
       return `${BUNDLED_BASE_URL}/portraits/${refname}.jpg`;
     case 'icon':
-      // Example: /images/icons/npc_dota_lone_druid_bear.png
+      // Example: ./images/icons/npc_dota_lone_druid_bear.png
       return `${BUNDLED_BASE_URL}/icons/${refname}.png`;
     default:
       return null;
