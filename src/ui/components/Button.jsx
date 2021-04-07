747faa6876fb48023efdb2b1b6e7ada58b485ca6
Tim Kurvers <tim@moonsphere.net>
2021-04-07 15:24:44 +0200

    feat: add fancy and disabled button styles

--- src/ui/components/Button.jsx
+++ src/ui/components/Button.jsx
@@ -1,18 +1,40 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const StyledButton = styled.button`
   appearance: none;
   background: none;
   border: none;
   color: #FFFFFF;
+  text-align: center;
+  font-size: 1rem;
   cursor: pointer;
-  font-size: 1.25em;
+
+  ${(props) => props.fancy && css`
+    background: #610606;
+    box-shadow: inset 1px 1px 0 0 #B60A0A, inset -1px -1px 0 0 #410404;
+    border-radius: 6px;
+    padding: 6px 40px;
+    font-family: 'Galdeano', sans-serif;
+    text-shadow: 1px 1px 0 #942020, -1px -1px 1px #000000;
+  `}
+
+  ${(props) => props.disabled && css`
+    filter: grayscale(50%);
+    opacity: .5;
+    cursor: not-allowed;
+  `}
 `;
 
-const Button = (props) => (
-  <StyledButton type="button" {...props} />
-);
+const Button = (props) => {
+  const { label, ...rest } = props;
+  const accessibility = {};
+  if (label) {
+    accessibility['aria-label'] = label;
+    accessibility.title = label;
+  }
+  return <StyledButton type="button" {...rest} {...accessibility} />;
+};
 
 export default Button;
 export { StyledButton };
