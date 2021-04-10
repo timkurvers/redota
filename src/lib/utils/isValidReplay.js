fe42a508fe7dc125bc81a29308ffe89a0e8acab7
Tim Kurvers <tim@moonsphere.net>
2021-04-10 21:14:47 +0200

    chore: isValidReplay accepts a blob

--- src/lib/utils/isValidReplay.js
+++ src/lib/utils/isValidReplay.js
@@ -1,10 +1,10 @@
 const MAGIC_SOURCE_2 = 'PBDEMS2\0';
 
-export default async (file) => {
+export default async (blob) => {
   const { length } = MAGIC_SOURCE_2;
-  if (file.size < length) {
+  if (blob.size < length) {
     return false;
   }
-  const magic = await file.slice(0, MAGIC_SOURCE_2.length).text();
+  const magic = await blob.slice(0, MAGIC_SOURCE_2.length).text();
   return magic === MAGIC_SOURCE_2;
 };
