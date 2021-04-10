538e257946e7916d28acd81f9dd840d5f33b0eee
Tim Kurvers <tim@moonsphere.net>
2021-04-10 15:54:12 +0200

    feat: index heroes by ID into definition at runtime

--- src/lib/definitions/index.js
+++ src/lib/definitions/index.js
@@ -1,5 +1,15 @@
-export { default as heroesByName } from 'dotaconstants/build/hero_names.json';
+/* eslint-disable no-param-reassign */
+
+import heroesByName from 'dotaconstants/build/hero_names.json';
+
 export { default as itemsByName } from 'dotaconstants/build/items.json';
 export { default as xpPerLevel } from 'dotaconstants/build/xp_level.json';
 
 export { default as unitsByName } from './unitsByName.js';
+
+const heroesByID = Object.values(heroesByName).reduce((lookup, hero) => {
+  lookup[hero.id] = hero;
+  return lookup;
+}, {});
+
+export { heroesByID, heroesByName };
