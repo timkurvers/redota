f143c5bbb04929cf43c849c30b397f6a64a49ca1
Tim Kurvers <tim@moonsphere.net>
2021-04-13 13:38:09 +0200

    feat: add fetchMatchRequest and fetchReplayRequest to API integrations

--- src/lib/api/index.js
+++ src/lib/api/index.js
@@ -1,11 +1,42 @@
+/* eslint-disable import/prefer-default-export */
+
+import axios, { CancelToken } from 'axios';
+
 const API_BASE = 'https://api.opendota.com/api';
+const REPLAY_PROXY_BASE = 'https://redota.timkurvers.workers.dev';
 
-export const fetchMatch = async (id) => {
-  const result = await fetch(`${API_BASE}/matches/${id}`);
-  return result.json();
+export const fetchRequest = (url, options = {}) => {
+  const source = CancelToken.source();
+
+  // Request returning an axios-backed Promise when invoked
+  const request = () => (
+    axios.request({
+      timeout: 10000,
+      method: 'get',
+      ...options,
+      cancelToken: source.token,
+      url,
+    })
+  );
+
+  // Attach cancellation function to request
+  request.cancel = () => {
+    source.cancel();
+  };
+
+  return request;
 };
 
-export const fetchPlayer = async (id) => {
-  const result = await fetch(`${API_BASE}/players/${id}`);
-  return result.json();
-};
+export const fetchMatchRequest = (id, options) => (
+  fetchRequest(`${API_BASE}/matches/${id}`, options)
+);
+
+export const fetchReplayRequest = (url, options) => (
+  fetchRequest(`${REPLAY_PROXY_BASE}/?replay=${url}`, {
+    ...options,
+    responseType: 'blob',
+
+    // Waive timeout as these replay files (50 MB+) may take a while to download
+    timeout: 0,
+  })
+);
