8f599d60cedd869adb89333492d0a95e66336e5d
Tim Kurvers <tim@moonsphere.net>
2023-04-21 21:05:36 +0200

    build: patch 7.33 support

--- src/ui/pages/templates/ContentPage.jsx
+++ src/ui/pages/templates/ContentPage.jsx
@@ -92,8 +92,8 @@ const ContentPage = (props) => {
   );
 
   const version = (
-    <Link to="https://github.com/timkurvers/redota/compare/v1.6.0...v1.7.0" grayscale>
-      v1.7.0
+    <Link to="https://github.com/timkurvers/redota/compare/v1.7.0...v1.8.0" grayscale>
+      v1.8.0
     </Link>
   );
 
