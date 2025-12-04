b378251de897a1e3a2c9febf8c33c1b7cdd24731
Tim Kurvers <tim@moonsphere.net>
2025-12-04 22:41:14 +0100

    build: overflow bug fix for readBitInt

--- src/ui/pages/templates/ContentPage.jsx
+++ src/ui/pages/templates/ContentPage.jsx
@@ -92,8 +92,8 @@ const ContentPage = (props) => {
   );
 
   const version = (
-    <Link to="https://github.com/timkurvers/redota/compare/v1.12.0...v1.12.1" grayscale>
-      v1.12.1
+    <Link to="https://github.com/timkurvers/redota/compare/v1.12.1...v1.12.2" grayscale>
+      v1.12.2
     </Link>
   );
 
