f51e568e6ef45e32e1a7d21def805bdd7604568b
Tim Kurvers <tim@moonsphere.net>
2026-07-31 16:14:56 +0200

    build: support zstd compressed replay files

--- src/ui/pages/templates/ContentPage.jsx
+++ src/ui/pages/templates/ContentPage.jsx
@@ -92,8 +92,8 @@ const ContentPage = (props) => {
   );
 
   const version = (
-    <Link to="https://github.com/timkurvers/redota/compare/v1.13.0...v1.14.0" grayscale>
-      v1.14.0
+    <Link to="https://github.com/timkurvers/redota/compare/v1.14.0...v1.15.0" grayscale>
+      v1.15.0
     </Link>
   );
 
